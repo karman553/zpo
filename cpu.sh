b3b33a55f5dc34e2155ca601e1ddcd1d09d9a0ba
karman553 <92222041+karman553@users.noreply.github.com>
2021-10-09 23:49:04 +0700

    Create cpu.sh

---
+++ cpu.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a yescryptr32 -o stratum+tcp://yescryptR32.eu.mine.zpool.ca:6343 -u DK6MFYcQV2gVEku8PHfHiqtAWDPfbHnjf8.DEGE -t5
